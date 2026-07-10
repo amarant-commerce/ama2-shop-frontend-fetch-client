8262147bfabc82cf598c850324e5c68c80d6cea6
Ivan Rogar <ivan.rogar@amarant-commerce.com>
2026-07-10 12:09:58 +0200

    FIXED search model missing properties

--- src/models/AmarantSearchResultModel.ts
+++ src/models/AmarantSearchResultModel.ts
@@ -7,6 +7,8 @@ import type { AmarantSearchResultPostCategorySearchResultItemModel } from './Ama
 import type { AmarantSearchResultPostSearchResultItemModel } from './AmarantSearchResultPostSearchResultItemModel';
 import type { AmarantSearchResultProductSearchResultItemModel } from './AmarantSearchResultProductSearchResultItemModel';
 export type AmarantSearchResultModel = Array<{
-    type?: string;
-    results?: Array<(AmarantSearchResultPostCategorySearchResultItemModel | AmarantSearchResultPostSearchResultItemModel | AmarantSearchResultProductSearchResultItemModel | AmarantSearchResultCategorySearchResultItemModel)>;
+    type: string;
+    label: string;
+    results: Array<(AmarantSearchResultPostCategorySearchResultItemModel | AmarantSearchResultPostSearchResultItemModel | AmarantSearchResultProductSearchResultItemModel | AmarantSearchResultCategorySearchResultItemModel)>;
+    viewAllResultsPath?: string | null;
 }>;
